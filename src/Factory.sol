9b9e77963e76193224b24b366e231990268723db
marcuswentz <markzcollege404@gmail.com>
2023-05-19 16:55:22 -0400

    Add INIT_CODE_PAIR_HASH to UniswapV2Factory.sol

--- src/Factory.sol
+++ src/Factory.sol
@@ -402,6 +402,8 @@ contract UniswapV2Factory is IUniswapV2Factory {
     mapping(address => mapping(address => address)) public getPair;
     address[] public allPairs;
 
+    bytes32 public constant INIT_CODE_PAIR_HASH = keccak256(abi.encodePacked(type(UniswapV2Pair).creationCode));
+
     event PairCreated(address indexed token0, address indexed token1, address pair, uint);
 
     constructor(address _feeToSetter) public {
